cd7b4324a7d9c9e93e875ec5f3d2bbe2d29c99f7
edunham <dunhame@onid.orst.edu>
2014-02-16 20:20:26 -0800

    magic numbers for gismu list line splitting

--- source/db.py
+++ source/db.py
@@ -28,3 +28,21 @@ class Gismu(Base):
     n2 = Column(Int)
     notes = Column(String)
     #cflist http://docs.sqlalchemy.org/en/rel_0_7/orm/relationships.html
+
+    def __init__(name, dfn, n1, n2, r1=None, r2=None, r3=None, mn=None,
+            notes=None)
+        pass
+
+    def slurp(path):
+        f = open(path)
+        for l in f.readlines():
+            name = l[1:6]
+            r1 = l[7:10].strip()
+            r2 = l[11:14].strip()
+            r3 = l[15:19].strip()
+            tr = l[20:41].strip()               # translation
+            mn = l[41:62].strip().strip("'")    # mneumonic
+            dfn = l[63:159].strip()
+            n1 = t[159:162].strip()
+            n2 = int(t[162:165].strip())
+            
